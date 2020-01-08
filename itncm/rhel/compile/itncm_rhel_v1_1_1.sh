8c1fd37d60b9b523320d15f22bc147991a1a27ed
cywisp <cywisp@gmail.com>
2020-01-08 10:16:43 -0500

    updated

--- itncm/rhel/compile/itncm_rhel_v1_1_1.sh
+++ itncm/rhel/compile/itncm_rhel_v1_1_1.sh
@@ -162,6 +162,7 @@ function os_info(){
 function installed_software(){
     rpm -qa | sort |tr [a-z] [A-Z] | while read app; do
         printf "C010_2_R1:1:2,%s\n" $app
+    done
 }
 
 function db2_application(){
