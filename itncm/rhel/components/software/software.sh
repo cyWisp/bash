76dc9fd87640deec675e71f8f496f3e4eeb5eb90
cywisp <cywisp@gmail.com>
2020-01-09 12:05:13 -0500

    updated

--- itncm/rhel/components/software/software.sh
+++ itncm/rhel/components/software/software.sh
@@ -3,9 +3,9 @@
 function installed_software(){
     rpm -qa | sort |tr [a-z] [A-Z] | while read app; do
         first_field=$(echo ${app} | awk -F "." '{print $1}')
-        display_name=${first_field::-2}
+        #display_name=${first_field::-2}
         
-        printf "C010_2_R1:1:2,%s\n" $display_name
+        printf "C010_2_R1:1:2,%s\n" $first_field
     done
 }
 
