1cdcb07663a6ddabdb55726ccb470a4bdec9c320
cywisp <cywisp@gmail.com>
2020-01-09 12:16:21 -0500

    updated

--- itncm/rhel/components/software/software.sh
+++ itncm/rhel/components/software/software.sh
@@ -2,12 +2,12 @@
 
 function installed_software(){
     rpm -qa | sort |tr [a-z] [A-Z] | while read app; do
-        first_field=$(echo ${app} | awk -F "." '{print $1}')
-        display_name=$(echo ${first_field//-})
-        architecture=$(echo ${app} | awk -F "." '{print $NF}')
+        # first_field=$(echo ${app} | awk -F "." '{print $1}')
+        # display_name=$(echo ${first_field//-})
+        # architecture=$(echo ${app} | awk -F "." '{print $NF}')
 
-
-        printf "C010_2_R1:1:2,%s,%s\n" $display_name $architecture
+        printf "C010_2_R1:1:2,%s\n" $app
+        #printf "C010_2_R1:1:2,%s,%s\n" $display_name $architecture
     done
 }
 
