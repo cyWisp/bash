cdb3d17868c7cf38507d697fe8912663fe622ebd
cywisp <cywisp@gmail.com>
2020-01-09 13:59:37 -0500

    updated

--- itncm/rhel/components/software/software.sh
+++ itncm/rhel/components/software/software.sh
@@ -8,7 +8,9 @@ function installed_software(){
         first_field=$(echo ${app} | awk -F "." '{print $1}')
         display_name=$(echo ${first_field//-})
         architecture=$(echo ${app} | awk -F "." '{print $NF}')
-        display_version=$(echo ${app} | awk -F "-" '{print $(NF-1)}' | awk -F "." '{print $1}')
+        dv_prefix=$(echo ${app} | awk -F "-" '{print $(NF-1)}')
+        dv_suffix=$(echo ${dv_prefix} | awk -F "." '{print $1}')
+        display_name="${dv_prefix}${dv_suffix}"
 
         printf "C010_2_R1:1:2,%s\n" $display_version
         #printf "C010_2_R1:1:2,%s,%s\n" $display_name $architecture
