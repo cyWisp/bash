7359cc2ad78a9ea58047f7ae3ccf3eb78b7f96d5
cywisp <cywisp@gmail.com>
2020-01-09 14:08:11 -0500

    updated

--- itncm/rhel/compile/itncm_rhel_v1_1_1.sh
+++ itncm/rhel/compile/itncm_rhel_v1_1_1.sh
@@ -159,9 +159,26 @@ function os_info(){
     printf "\n###__GPU__END ###\n\n"
 }
 
+# function installed_software(){
+#     rpm -qa | sort |tr [a-z] [A-Z] | while read app; do
+#         printf "C010_2_R1:1:2,%s\n" $app
+#     done
+# }
+
 function installed_software(){
+    host_name=$(hostname)
+
+    printf "C010_2_R1:1:2,host_name,display_name,display_version,architecture\n"
     rpm -qa | sort |tr [a-z] [A-Z] | while read app; do
-        printf "C010_2_R1:1:2,%s\n" $app
+        first_field=$(echo ${app} | awk -F "." '{print $1}')
+        dv_prefix=$(echo ${app} | awk -F "-" '{print $(NF-1)}')
+        dv_suffix=$(echo ${dv_prefix} | awk -F "." '{print $1}')
+
+        display_name=$(echo ${first_field//-})
+        architecture=$(echo ${app} | awk -F "." '{print $NF}')
+        display_version="${dv_prefix}${dv_suffix}"
+
+        printf "C010_2_R1:1:2,%s,%s,%s,%s\n" $host_name $display_name $display_version $architecture 
     done
 }
 
--- itncm/rhel/components/software/software.sh
+++ itncm/rhel/components/software/software.sh
@@ -3,7 +3,7 @@
 function installed_software(){
     host_name=$(hostname)
 
-    printf "C010_2_R1:1:2,host_name,display_name,display_version,architecture"
+    printf "C010_2_R1:1:2,host_name,display_name,display_version,architecture\n"
     rpm -qa | sort |tr [a-z] [A-Z] | while read app; do
         first_field=$(echo ${app} | awk -F "." '{print $1}')
         dv_prefix=$(echo ${app} | awk -F "-" '{print $(NF-1)}')
@@ -14,7 +14,6 @@ function installed_software(){
         display_version="${dv_prefix}${dv_suffix}"
 
         printf "C010_2_R1:1:2,%s,%s,%s,%s\n" $host_name $display_name $display_version $architecture 
-        #printf "C010_2_R1:1:2,%s,%s\n" $display_name $architecture
     done
 }
 
