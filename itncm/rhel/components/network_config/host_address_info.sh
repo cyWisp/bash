059afa4a7c763d6b2f23e29d0f93a65c43c924c4
cywisp <cywisp@gmail.com>
2020-01-14 10:59:24 -0500

    updated

--- itncm/rhel/components/network_config/host_address_info.sh
+++ itncm/rhel/components/network_config/host_address_info.sh
@@ -1,36 +1,53 @@
 #!/bin/bash
-function get_network_info(){
-        printf "hostname,ip_address,mac_address\n"
-        adapters=$(nmcli device status | sed -n '1!p' | awk '{print $1}')
+# function get_network_info(){ #Using nmcli
+#         printf "hostname,ip_address,mac_address\n"
+#         adapters=$(nmcli device status | sed -n '1!p' | awk '{print $1}')
         
-        for a in $adapters; do
-            if [ "$a" = "lo" ] || [ "$a" = "p2p-dev-wlp2s0" ]; then
-                    continue
-            else
-                hostname=$(hostname)
-                ip_address=$(ip addr | grep ${a} | grep inet | cut -d "/" -f 1 | awk '{print $2}')
-                mac_address=$(ip addr | grep ${a} -A1 | grep ether | awk '{print $2}')
+#         for a in $adapters; do
+#             if [ "$a" = "lo" ] || [ "$a" = "p2p-dev-wlp2s0" ]; then
+#                     continue
+#             else
+#                 hostname=$(hostname)
+#                 ip_address=$(ip addr | grep ${a} | grep inet | cut -d "/" -f 1 | awk '{print $2}')
+#                 mac_address=$(ip addr | grep ${a} -A1 | grep ether | awk '{print $2}')
 
-                printf "HOST:IP:MAC,%s,%s,%s\n" $hostname $ip_address $mac_address
-            fi
-        done
-}
+#                 printf "HOST:IP:MAC,%s,%s,%s\n" $hostname $ip_address $mac_address
+#             fi
+#         done
+# }
 
-function enumerate_adatpers(){
+function get_network_info(){ # Using netstat
     printf "hostname,ip_address,mac_address\n"
-    adapters=$(netstat -i | column -t | awk '/Iface/{getline; print}' | awk '{print $1}')
-    for a in $adapters; do
-        if [ "$a" = "lo" ] || [ "$a" = "p2p-dev-wlp2s0" ]; then
+    #ns_enum=$(netstat -i | column -t | awk '/Iface/{getline; print}' | awk '{print $1}')
+    
+    adapters=()
+    counter=0
+    ns_enum=$(netstat -i | column -t)
+    
+    echo "${ns_enum}" | while read output_line; do
+        interface=$(echo "${output_line}" | awk '{print $1}')
+        if [ "$interface" = "Kernel" ] || [ "$interface" = "Iface" ]; then
             continue
         else
-            hostname=$(hostname)
-            ip_address=$(ip addr | grep ${a} | grep inet | cut -d "/" -f 1 | awk '{print $2}')
-            mac_address=$(ip addr | grep ${a} -A1 | grep ether | awk '{print $2}')
+            adapters[counter]=$interface
+            counter+=1
+     
+    echo "${adapters[@]}"
 
-            printf "HOST:IP:MAC,%s,%s,%s\n" $hostname $ip_address $mac_address
-        fi
-    done
+    # for a in $adapters; do
+    #     if [ "$a" = "lo" ] || [ "$a" = "p2p-dev-wlp2s0" ]; then
+    #         continue
+    #     else
+    #         hostname=$(hostname)
+    #         ip_address=$(ip addr | grep ${a} | grep inet | cut -d "/" -f 1 | awk '{print $2}')
+    #         mac_address=$(ip addr | grep ${a} -A1 | grep ether | awk '{print $2}')
+
+    #         printf "HOST:IP:MAC,%s,%s,%s\n" $hostname $ip_address $mac_address
+    #     fi
+    # done
 }
+
+
 
 #get_network_info
 enumerate_adatpers
