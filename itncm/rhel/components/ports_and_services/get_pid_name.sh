0acea4a4b1f308744cd86a77ad0c70526ea8637e
cywisp <cywisp@gmail.com>
2020-01-15 10:56:02 -0500

    updated

--- itncm/rhel/components/ports_and_services/get_pid_name.sh
+++ itncm/rhel/components/ports_and_services/get_pid_name.sh
@@ -3,6 +3,25 @@
 netstat_output=$(sudo netstat -antp | grep LISTEN)
 
 echo "${netstat_output}" | while read line; do
+    protocol_type=$(echo $line | awk '{print $1}')
+    port_status=$(echo $line | awk '{print $6}')
     pid_num=$(echo ${line} | awk '{print $7}' | awk -F "/" '{print $1}')
-    echo "$pid_num"
+    process_name=$(ps aux | grep ${pid_num} | awk '{print $11}')
+    if [ "$port_status" = "LISTEN" ]; then
+        if [ "$protocol_type" != "tcp" ]; then
+            port=$(echo $line | awk '{print $4}' | cut -d ":" -f 4)
+            ip_type="IPv6"    
+        else
+            port=$(echo $line | awk '{print $4}' | cut -d ":" -f 2)
+            ip_type="IPv4"
+        fi
+
+        if [ "$process_name" = "-" ]; then
+            $process_name="n/a"
+        else
+            :
+        fi
+            
+        printf "%s,%s,%s,%s,%s\n" $host_name $protocol_type $port $ip_type $process_name
+    fi
 done
