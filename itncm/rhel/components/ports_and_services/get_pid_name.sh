a231241fc84be92e8c89a5ca7c0a7e334c986f00
cywisp <cywisp@gmail.com>
2020-01-15 10:50:16 -0500

    updated

--- itncm/rhel/components/ports_and_services/get_pid_name.sh
+++ itncm/rhel/components/ports_and_services/get_pid_name.sh
@@ -3,5 +3,6 @@
 netstat_output=$(sudo netstat -antp | grep LISTEN)
 
 echo "${netstat_output}" | while read line; do
-    echo "${line}"
+    pid_num=$(echo ${line} | awk '{print $7}' | awk -F "/" '{print $1}')
+    echo "$pid_num"
 done
