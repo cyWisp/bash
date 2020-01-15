cac06b8eb7e772c168f30d305b371b4875977914
cywisp <cywisp@gmail.com>
2020-01-15 11:15:59 -0500

    updated

--- itncm/rhel/components/ports_and_services/get_pid_name.sh
+++ itncm/rhel/components/ports_and_services/get_pid_name.sh
@@ -3,12 +3,12 @@
 hostname=$(hostname)
 netstat_output=$(sudo netstat -antp | grep "LISTEN")
 
-echo "${netstat_output}" | while read line; do
-    protocol_type=$(echo $line | awk '{print $1}')
-    port_status=$(echo $line | awk '{print $6}')
-    pid_num=$(echo ${line} | awk '{print $7}' | awk -F "/" '{print $1}')
-    process_name=$(ps aux | grep "${pid_num}" -m 1 | awk '{print $11}')
-    #if [ "$port_status" = "LISTEN" ]; then
+function ports_and_services(){
+    echo "${netstat_output}" | while read line; do
+        protocol_type=$(echo $line | awk '{print $1}')
+        port_status=$(echo $line | awk '{print $6}')
+        pid_num=$(echo ${line} | awk '{print $7}' | awk -F "/" '{print $1}')
+        process_name=$(ps aux | grep "${pid_num}" -m 1 | awk '{print $11}')
         if [ "$protocol_type" != "tcp" ]; then
             port=$(echo $line | awk '{print $4}' | cut -d ":" -f 4)
             ip_type="IPv6"    
@@ -17,12 +17,14 @@ echo "${netstat_output}" | while read line; do
             ip_type="IPv4"
         fi
 
-        # if [ "$process_name" = "-" ]; then
-        #     $process_name="n/a"
-        # else
-        #     :
-        # fi
-            
-        printf "%s,%s,%s,%s %s\n" $hostname $protocol_type $port $ip_type $process_name
-    #fi
-done
+        if [ "$process_name" = "-" ]; then
+            $process_name="n/a"
+        else
+            :
+        fi
+                
+        printf "%s,%s,%s,%s,%s\n" $hostname $protocol_type $port $ip_type $process_name
+    done
+}
+
+ports_and_services
