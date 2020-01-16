f368ad71ad1825e98482a95eee1de3df1e9ab82f
cywisp <cywisp@gmail.com>
2020-01-16 14:03:37 -0500

    updated

--- itncm/rhel/components/software/software_formatting.sh
+++ itncm/rhel/components/software/software_formatting.sh
@@ -12,6 +12,8 @@ function installed_software(){
         #architecture=$(echo ${app} | awk -F "." '{print $NF}')
         #display_version="${dv_prefix}${dv_suffix}"
 
+        printf "%s\n" $display_name
+
         #printf "C010_2_R1:1:2,%s,%s,%s,%s\n" $host_name $display_name $display_version $architecture 
     done
 }
