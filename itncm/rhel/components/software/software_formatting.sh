2b35bf8cb5a557648e62882685246525e843121d
cywisp <cywisp@gmail.com>
2020-01-16 14:08:34 -0500

    updated

--- itncm/rhel/components/software/software_formatting.sh
+++ itncm/rhel/components/software/software_formatting.sh
@@ -4,17 +4,16 @@ function installed_software(){
 
     printf "C010_2_R1:1:2,host_name,display_name,display_version,architecture\n"
     rpm -qa | sort |tr [a-z] [A-Z] | while read app; do
-        first_field=$(echo ${app} | awk -F "." '{print $1}' | awk -F "-" '{print $1}')
-        #dv_prefix=$(echo ${app} | awk -F "-" '{print $(NF-1)}')
-        #dv_suffix=$(echo ${dv_prefix} | awk -F "." '{print $1}')
+        display_name=$(echo ${app} | awk -F "." '{print $1}' | awk -F "-" '{print $1}')
+        dv_prefix=$(echo ${app} | awk -F "-" '{print $(NF-1)}')
+        dv_suffix=$(echo ${dv_prefix} | awk -F "." '{print $1}')
 
-        #display_name=$(echo ${first_field//-})
-        #architecture=$(echo ${app} | awk -F "." '{print $NF}')
-        #display_version="${dv_prefix}${dv_suffix}"
+        architecture=$(echo ${app} | awk -F "." '{print $NF}')
+        display_version="${dv_prefix}${dv_suffix}"
 
         printf "%s\n" $first_field
 
-        #printf "C010_2_R1:1:2,%s,%s,%s,%s\n" $host_name $display_name $display_version $architecture 
+        printf "C010_2_R1:1:2,%s,%s,%s,%s\n" $host_name $display_name $display_version $architecture 
     done
 }
 
