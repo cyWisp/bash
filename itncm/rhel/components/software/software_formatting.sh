a09f66d1c7a5919fef3b2257d0205b55ecca086f
cywisp <cywisp@gmail.com>
2020-01-16 14:22:26 -0500

    updated

--- itncm/rhel/components/software/software_formatting.sh
+++ itncm/rhel/components/software/software_formatting.sh
@@ -4,7 +4,7 @@ function installed_software(){
 
     printf "C010_2_R1:1:2,host_name,display_name,display_version,architecture\n"
     rpm -qa | sort |tr [a-z] [A-Z] | while read app; do
-        display_name=$(echo ${app} | awk -F "." '{print $1}' | sed 's/[0-9]//g' | awk -F "-" '{print $NF=""}')
+        display_name=$(echo ${app} | awk -F "." '{print $1}' | sed 's/[0-9]//g')
         dv_prefix=$(echo ${app} | awk -F "-" '{print $(NF-1)}')
         dv_suffix=$(echo ${dv_prefix} | awk -F "." '{print $1}')
 
