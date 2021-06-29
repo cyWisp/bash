bbded558e49b02e339a6f8ef164e5284074d2251
cywisp <cywisp@gmail.com>
2021-06-29 11:34:22 -0400

    UPDATED install docker

--- install_docker/install_docker.sh
+++ install_docker/install_docker.sh
@@ -3,20 +3,26 @@
 # This script will set up the Docker repository
 # and install Docker on CentOS 7
 
-admin=""
+admin=$1
 docker_repo="https://download.docker.com/linux/centos/docker-ce.repo"
 log_file="./install.log"
 
 # Install yum-utils
 printf "[!] Installing yum-utils...\n" >> ${log_file}
-echo ${admin} | sudo -S yum install -y yum-utils
+echo ${admin} | sudo -S yum install -y yum-utils >> ${log_file}
 
 printf "[!] Adding Docker repository...\n" >> ${log_file}
-sudo yum-config-manager --add-repo ${docker_repo}
+sudo yum-config-manager --add-repo ${docker_repo} >> ${log_file}
 
 printf "[!] Installing docker-ce, docker-cd-cli and containerd.io\n" >> ${log_file}
-sudo yum install -y docker-ce docker-cd-cli containerd.io
+sudo yum install -y docker-ce docker-cd-cli containerd.io >> ${log_file}
 
-printf "[!] Got here!"
+printf "[!] Initializing post-installation tasks...\n"
+printf "[!] Creating docker group...\n"
+sudo groupadd docker
 
+printf "[!] Adding user ${admin} to docker group...\n"
+sudo usermod -aG docker ${admin}
 
+printf "[!] Refreshing group membership..."
+newgrp docker
---
+++ install_docker/tests/cmd_args.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+name=$1
+age=$2
+
+echo "Hi, my name is ${name} and I am ${age} years old..."
