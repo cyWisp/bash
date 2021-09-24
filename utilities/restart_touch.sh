9d23279de203dacefe86b1eccf49187051829b6d
cywisp <cywisp@gmail.com>
2021-09-24 09:45:59 -0400

    added comments to restart_touch.sh

--- utilities/restart_touch.sh
+++ utilities/restart_touch.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
+# This script queries the psmouse (touchpad device)
+# on an ubuntu based laptop and restarts the driver
+
+# Gather superuser credentials
+# and query the device
 read -p "[?] Pass: " -s pass
-
-
 echo "${pass}" | sudo -S modprobe -r psmouse
 
+# Start the driver
 echo "[+] Restarting touchpad..."
 sudo modprobe psmouse
 
+# If there's an error, notify
+# else present a confirmation message
 if [ $? -ne 0 ]; then
 	echo "[x] Something went wrong..."
 else
