92ff28413e088e6fdcc69f71a8be1e861d83a47c
cywisp <cywisp@gmail.com>
2024-12-30 14:20:29 -0500

    Updated vite project scaffolder

--- utilities/vite_project/create_vite_project.sh
+++ utilities/vite_project/create_vite_project.sh
@@ -1,4 +1,40 @@
-#!/usr/bin/env bash
+#!/usr/bin/env zsh
+
+MAIN_FILE_CONTENT=`cat <<EOF
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
+EOF
+`
+
+APP_FILE_CONTENT=`cat <<EOF
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <div>
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default App
+EOF
+`
+
 
 function log () {
   echo "${BASHPID} $(date '+%d-%m-%Y %H:%M:%S'): $1 $2"
@@ -31,9 +67,36 @@ function initialize () {
   check_error $? "dependency installation"
 }
 
+function refine_folder_structure () {
+  log "Refining directory structure."
+
+  log "Removing default css templates."
+  rm src/*.css src/*.tsx
+
+  log "Creating assets directories"
+  mkdir "src/assets/css" "src/assets/images"
+  rm "src/assets/react.svg"
+
+  log "Creating components folder."
+  mkdir "src/components"
+
+  check_error $? "folder structure refinement"
+
+}
+
+function create_file_templates () {
+  log "Creating custom file templates."
+  echo $MAIN_FILE_CONTENT > src/main.tsx
+  echo $APP_FILE_CONTENT > src/App.tsx
+
+  check_error $? "custom template creation"
+}
+
 
 validate_user_input $1
 initialize $1
+refine_folder_structure
+create_file_templates
 
 npm run dev
 
